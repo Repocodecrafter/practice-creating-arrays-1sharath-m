810018344785ac2cdd2e3df3bab9e75e1f79d978
Sharath Meghana <marellameghana99@gmail.com>
2025-10-16 00:44:26 -0500

    Create and log grocery items and price arrays for practice

---
+++ practice-creating-arrays-1.js
@@ -0,0 +1,14 @@
+//Task 1
+let groceryList = ["Milk", "Bread", "Eggs", "oil", "Sugar"];
+let prices = [100.00, 159.99, 299.99];
+
+
+
+//Task2
+console.log("Grocery Items: ", groceryList);
+console.log("Prices: ", prices);
+console.log("Item 1:", groceryList[1]);
+console.log("Item 2:", groceryList[3]);
+console.log("Item 3:", groceryList[4]);
+console.log("Price 1:", prices[0]);
+console.log("Price 2:", prices[2]);
